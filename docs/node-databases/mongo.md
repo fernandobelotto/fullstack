a34fb75a29487e2f45d734a0a9b8af5f8ef30682
Fernando Bosco <fbosco@dlocal.com>
2022-05-29 22:10:30 -0300

    initial

---
+++ docs/node-databases/mongo.md
@@ -0,0 +1,60 @@
+# Node + Mongo
+
+Para conectar o node com o mongo vamos usar o docker para facilitar o processo, já que só instalando o docker conseguimos iniciar o mongo sem a necessidade de instalá-lo.
+
+## Passo a passo
+
+1. Instale o docker na sua máquina usando esse
+   **[link do site do docker](https://www.docker.com/get-started/)**
+
+1. Instale o mongo compass (uma ferramenta para gerenciar mongodb) **[nesse link](https://www.mongodb.com/download-center/compass)**
+
+1. Para iniciar o docker abra seu terminal e execute o comando a seguir. Ele vai criar um container do docker, que vai rodar o mongo.
+
+   ```
+   docker run --name some-mongo -d mongo:latest
+   ```
+
+1. Depois, para se conectar ao mongo usando a cli dele, abra o terminal e execute o comando a seguir.
+
+   ```
+   docker exec -it some-mongo mongo
+   ```
+
+1. Agora vamos ver as databases que temos
+
+      ````
+      show dbs
+      ````
+
+1. Vamos criar uma database chamada letscode:
+
+      ````
+      use letscode
+      ````
+
+1. Pronto, agora temos uma database, com uma tabela e uma entry nessa tabela. Vamos ao código!
+
+      ```
+      db.users.insertOne({
+         name: "John",
+         age: 30,
+         city: "New York",
+         state: "NY",
+         country: "USA",
+         address: {
+            street: "Wall Street",
+            zip: "10001",
+            building: "1A",
+         },
+         categories: ["books", "movies", "music", "games"],
+      })
+      ```
+
+1. Para verificar agora com a coleção criada com um objeto, rode o comando:
+
+      ````
+      db.users.find().pretty()
+      ````
+
+1. Perfeito, vamos ao código agora!
---
+++ docusaurus.config.js
@@ -0,0 +1,163 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: `Let's Code Web Full Stack`,
+  tagline: 'A sua jornada começa aqui!',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'fernandobelotto', // Usually your GitHub org/user name.
+  projectName: 'fullstack', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  plugins: ['@docusaurus/theme-live-codeblock', [
+    '@docusaurus/plugin-pwa',
+    {
+      debug: true,
+      offlineModeActivationStrategies: [
+        'appInstalled',
+        'standalone',
+        'queryString',
+      ],
+      pwaHead: [
+        {
+          tagName: 'link',
+          rel: 'icon',
+          href: '/img/docusaurus.png',
+        },
+        {
+          tagName: 'link',
+          rel: 'manifest',
+          href: '/manifest.json', // your PWA manifest
+        },
+        {
+          tagName: 'meta',
+          name: 'theme-color',
+          content: 'rgb(37, 194, 160)',
+        },
+      ],
+    },
+  ],],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Web Full Stack',
+        logo: {
+          alt: 'logo do site',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      liveCodeBlock: {
+        playgroundPosition: 'bottom',
+      },
+    }),
+};
+
+module.exports = config;
